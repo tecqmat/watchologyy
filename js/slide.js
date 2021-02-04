4d1cf098d0e59b277b5d766025e5731719af24d8
tecqmat <tecqmat@gmail.com>
2021-02-04 07:07:07 +0530

    improved slider

--- js/slide.js
+++ js/slide.js
@@ -1,23 +1,41 @@
-var slideIndex = 1;
+(function(window, document){
+  var slideIndex = 0;
+  var slides = null;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-
-  if (n > slides.length) {slideIndex = 1;}    
-  if (n < 1) {slideIndex = slides.length;}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].classList.remove('active');  
+  function showSlides(n) {
+    slideIndex += n;
+    slideIndex = 0 > slideIndex ? slides.length - 1 : slideIndex;
+    slideIndex = slideIndex % slides.length;
+    slides.forEach(function(slide) {
+        slide.classList.remove('active');  
+    });
+    slides[slideIndex].classList.add('active');  
   }
-  slides[slideIndex-1].classList.add('active');  
-}
 
-showSlides(slideIndex);
+  function handle(e, n){
+    e.preventDefault();
+    e.stopPropagation();
+    showSlides(n);
+  }
+
+  function navSlide(el, n, box, key){
+    el.addEventListener('click', function(e){
+      handle(e, n);
+    });
+
+    box.addEventListener('keyup', function(e){
+      var kc = e.keyCode || e.which;
+      if(key === kc){
+        handle(e, n);
+      }
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded',function(){
+    slides = document.querySelectorAll(".mySlides");
+    showSlides(slideIndex);
+    var box = document.querySelector('body');
+    navSlide(document.querySelector('.prev'), -1, box, 37);
+    navSlide(document.querySelector('.next'), 1, box, 39);
+  });
+})(window, document);
